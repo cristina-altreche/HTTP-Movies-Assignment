2046a84c97036a34f77b4355d39793e386231ba8
Cristina Altreche <c.altreche@gmail.com>
2020-07-22 15:13:26 -0400

    Route to update movies added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovieForm from "./Movies/UpdateMovieForm";
 import axios from 'axios';
 
 const App = () => {
@@ -35,6 +36,7 @@ const App = () => {
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
+      <Route path="/update-movie/:id" render={()=> <UpdateMovieForm movieList={movieList} setMovieList={setMovieList}/>}/>
     </>
   );
 };
